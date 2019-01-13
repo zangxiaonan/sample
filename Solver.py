ebb08a2ca27bc1e9000c3070250f24a8601f02a4
hadoop <zangxiaonan@biconjs.com>
2019-01-13 13:59:08 +0800

    全部commit，测试用

---
+++ Solver.py
@@ -0,0 +1,20 @@
+import math
+import django
+
+class Solver(object):
+    def demo(self, a, b, c):
+        if ((a + b - c * c) > 0):
+            print(math.sqrt(c - 4 + 2))
+        else:
+            print("a")
+
+            raise Exception("数值为负数！")
+
+
+Solver().demo(2, 3, 1)
+Solver().demo(3, 4, 1)
+
+class Apple(object):
+    pass
+
+
